69b0ebe6d3d79f2afdfb9ca98534f407a8869c48
QuocTuanIT <kutuanonline199@gmail.com>
2023-10-15 17:35:03 +0700

    Customers by Stage: Tabs with Numbers

--- app/Filament/Resources/CustomerResource/Pages/ListCustomers.php
+++ app/Filament/Resources/CustomerResource/Pages/ListCustomers.php
@@ -3,7 +3,10 @@
 namespace App\Filament\Resources\CustomerResource\Pages;
 
 use App\Filament\Resources\CustomerResource;
+use App\Models\Customer;
+use App\Models\PipelineStage;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListCustomers extends ListRecords
@@ -15,5 +18,33 @@ class ListCustomers extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    public function getTabs(): array
+    {
+        $tabs = [];
+
+        // Adding `all` as our first tab
+        $tabs['all'] = Tab::make('All Customers')
+            // We will add a badge to show how many customers are in this tab
+            ->badge(Customer::count());
+
+        // Load all Pipeline Stages
+        $pipelineStages = PipelineStage::orderBy('position')->withCount('customers')->get();
+
+        // Loop through each Pipeline Stage
+        foreach ($pipelineStages as $pipelineStage) {
+            // Add a tab for each Pipeline Stage
+            // Array index is going to be used in the URL as a slug, so we transform the name into a slug
+            $tabs[str($pipelineStage->name)->slug()->toString()] = Tab::make($pipelineStage->name)
+                // We will add a badge to show how many customers are in this tab
+                ->badge($pipelineStage->customers_count)
+                // We will modify the query to only show customers in this Pipeline Stage
+                ->modifyQueryUsing(function ($query) use ($pipelineStage) {
+                    return $query->where('pipeline_stage_id', $pipelineStage->id);
+                });
+        }
+
+        return $tabs;
     }
 }
